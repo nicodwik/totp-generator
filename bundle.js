2f6afe36fb57389e075e41da597f5813dd19380b
nicodwi_k <nicodwika@gmail.com>
2024-10-01 19:45:08 +0700

    feat: add style

--- bundle.js
+++ bundle.js
@@ -1147,7 +1147,13 @@ break;case "inversionMode":switch(c){case "original":Y="dontInvert";break;case "
   var fetchAllCreds = () => {
     const creds = Object.entries(localStorage);
     const savedCredEl = document.getElementById("saved-credentials");
+    const emptyCreds = document.getElementById("empty-credentials");
     savedCredEl.textContent = "";
+    if (creds.length == 0) {
+      emptyCreds.classList.remove("d-none");
+      return;
+    }
+    emptyCreds.classList.add("d-none");
     creds.forEach((cred) => {
       const parsedCred = JSON.parse(cred[1]);
       const li = document.createElement("li");
@@ -1155,26 +1161,45 @@ break;case "inversionMode":switch(c){case "original":Y="dontInvert";break;case "
       const expiredTime = totp.expires;
       let secondsLeft = Math.ceil(Math.abs(expiredTime - (/* @__PURE__ */ new Date()).getTime()) / 1e3) - 1;
       const p = document.createElement("p");
+      p.style.wordBreak = "break-all";
       p.innerText = parsedCred.name;
+      p.setAttribute("class", "text-dark font-weight-bold text-center text-wrap");
+      const row = document.createElement("div");
+      row.setAttribute("class", "row justify-content-center");
       const textSecondsLeft = document.createElement("span");
-      textSecondsLeft.style.width = "10%";
       textSecondsLeft.style.marginRight = "10px";
+      textSecondsLeft.style.paddingTop = "9px";
       textSecondsLeft.innerText = secondsLeft;
+      textSecondsLeft.setAttribute("class", "badge badge-pill badge-secondary my-2 p-2");
       const input = document.createElement("input");
       input.setAttribute("disabled", true);
       input.setAttribute("value", totp.otp);
-      input.style.width = "30%";
-      input.style.height = "30px";
+      input.style.width = "50%";
       input.style.textAlign = "center";
       input.style.fontSize = "20px";
+      input.setAttribute("class", "form-control");
       const buttonCopy = document.createElement("button");
-      buttonCopy.style.width = "20%";
+      buttonCopy.style.width = "15%";
       buttonCopy.style.marginLeft = "10px";
-      buttonCopy.style.height = "30px";
-      buttonCopy.innerText = "copy";
+      buttonCopy.innerText = "Copy";
+      buttonCopy.setAttribute("class", "btn btn-dark");
       buttonCopy.addEventListener("click", async () => {
         await navigator.clipboard.writeText(input.value);
         alert("code copied to clipboard!");
+      });
+      const buttonDelete = document.createElement("button");
+      buttonDelete.style.width = "15%";
+      buttonDelete.style.marginLeft = "10px";
+      buttonDelete.innerText = "Delete";
+      buttonDelete.setAttribute("class", "btn btn-danger");
+      buttonDelete.setAttribute("data-id", parsedCred.secret);
+      buttonDelete.addEventListener("click", async () => {
+        if (confirm("Are you sure to delete this credential?")) {
+          const dataId = buttonDelete.getAttribute("data-id");
+          localStorage.removeItem(`cred_${dataId}`);
+          fetchAllCreds();
+          alert("credential deleted successfully");
+        }
       });
       setInterval(() => {
         textSecondsLeft.innerText = secondsLeft--;
@@ -1185,10 +1210,13 @@ break;case "inversionMode":switch(c){case "original":Y="dontInvert";break;case "
           secondsLeft = Math.ceil(Math.abs(newExpiredTime - (/* @__PURE__ */ new Date()).getTime()) / 1e3) - 1;
         }
       }, 1e3);
+      li.setAttribute("class", "list-group-item");
       li.append(p);
-      li.append(textSecondsLeft);
-      li.append(input);
-      li.append(buttonCopy);
+      li.append(row);
+      row.append(textSecondsLeft);
+      row.append(input);
+      row.append(buttonCopy);
+      row.append(buttonDelete);
       savedCredEl.appendChild(li);
     });
   };
